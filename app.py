969f01344058f31f9b941faf7b3dbeb8454e9aee
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-02-03 19:28:50 +0000

    another iteration

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def split(txt, seps):
     default_sep = seps[0]
     for sep in seps[1:]:
         txt = txt.replace(sep, default_sep)
-    return [i.strip() for i in txt.split()]
+    return [float(i.strip()) for i in txt.split()]
 
 @app.route('/')
 def index():
