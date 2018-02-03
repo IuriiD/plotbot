4cec4b2bf5040965243bfb708bad37c604a6f804
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-02-03 16:06:56 +0000

    another iteration

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ chart_name = 'Fibonacci'
 data = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 png_file_name = 'bar_chart1.png'
 
-def pygal_bar_chart(chart_name='Default', data, png_file_name):
+def pygal_bar_chart(data, png_file_name, chart_name='Default'):
     bar_chart = pygal.Bar()             # Then create a bar graph object
     bar_chart.add(chart_name, data)     # Add some values
     bar_chart.render_to_png(png_file_name)
